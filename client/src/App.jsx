b2119172eaf9d55e30fd20aa3dd3482862822e19
Ihtisham ul haq <158046842+Ihtisham304@users.noreply.github.com>
2024-10-15 11:25:43 +0500

    initialmproject

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+
+    </QueryClientProvider>
+  )
+};
+
+export default App;
